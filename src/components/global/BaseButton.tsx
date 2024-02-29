3e9bd12291b0447c1929af66312bbf6d24f9fd21
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-02-29 16:04:03 -0400

    Add new variant
    Foi adicionada a variante em que o botão simplesmente acompanha o tamanho do conteúdo, possuindo um width variável

--- src/components/global/BaseButton.tsx
+++ src/components/global/BaseButton.tsx
@@ -4,12 +4,13 @@ const variants = {
   base: 'bg-primary_600 w-3/4 h-14 rounded-3xl items-center justify-center mt-5 ',
   confirmation: 'bg-green-700 w-3/4 h-14 rounded-2xl items-center justify-center mt-5 ',
   selector: 'bg-primary_800 w-full h-14 rounded-xl items-center justify-between flex-row mt-5 px-2',
+  minimal_size: 'bg-primary_800 w-content px-8 h-14 rounded-xl items-center justify-center mt-5',
   selector_secondary:
     'bg-primary_700 w-full h-14 rounded-xl items-center justify-between flex-row mt-2 px-2',
 };
 
 interface ButtonProps extends TouchableOpacityProps {
-  variant?: 'confirmation' | 'base' | 'selector' | 'selector_secondary';
+  variant?: 'confirmation' | 'base' | 'selector' | 'selector_secondary' | 'minimal_size';
   titleClassName?: string;
   title: string;
   disabled?: boolean;
