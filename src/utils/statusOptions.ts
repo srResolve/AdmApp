35e435e6fa9329f3b7ae0e428d7830667f201844
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-04 11:09:26 -0400

    Status formatter

---
+++ src/utils/statusOptions.ts
@@ -0,0 +1,22 @@
+export const BudgetStatusOptions = [
+  {
+    label: 'Ver Todos',
+    value: 'all',
+  },
+  {
+    label: 'Pendente',
+    value: 'PENDING',
+  },
+  {
+    label: 'Aprovado',
+    value: 'APPROVED',
+  },
+  {
+    label: 'Pago',
+    value: 'PAYED',
+  },
+  {
+    label: 'Recusado',
+    value: 'REFUSED',
+  },
+];
