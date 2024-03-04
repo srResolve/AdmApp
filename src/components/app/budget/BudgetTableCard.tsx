e492a6f816ab68f989ef65ee340df4c72f0f9dab
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-04 13:32:16 -0400

    Create BudgetTableCard.tsx

---
+++ src/components/app/budget/BudgetTableCard.tsx
@@ -0,0 +1,31 @@
+import { Text, View, ViewProps } from 'react-native';
+import { StatusCard } from '../../global/StatusCard';
+
+interface Props extends ViewProps {
+  item: {
+    client: {
+      name: string;
+      address: string;
+    };
+    status: 'PENDING' | 'APPROVED' | 'REFUSED' | 'COMPLETE';
+  };
+}
+
+export function BudgetTableCard({ item, ...rest }: Props) {
+  return (
+    <View
+      className="flex-row justify-between p-1 px-2 items-center bg-primary_400 mt-2 rounded-lg border border-zinc-100"
+      {...rest}
+    >
+      <View className="w-4/6">
+        <Text numberOfLines={1} className="text-zinc-100 font-semibold text-lg">
+          {item.client.name}
+        </Text>
+        <Text numberOfLines={1} className="text-zinc-800 font-semibold">
+          {item.client.address}
+        </Text>
+      </View>
+      <StatusCard status={item.status} />
+    </View>
+  );
+}
