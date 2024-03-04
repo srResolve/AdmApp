073bad8cc52ffd96574d591a76c8b165e9fd27fe
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-04 15:18:58 -0400

    Change the way to select date

--- src/components/global/DatePickerModal.tsx
+++ src/components/global/DatePickerModal.tsx
@@ -1,39 +1,51 @@
+import { Feather } from '@expo/vector-icons';
+import moment from 'moment';
+import { useState } from 'react';
 import { Modal, Text, View } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { BackButton } from './BackButton';
-import * as Button from './Button';
-import { BaseInput } from './input/BaseInput';
+import { BaseButton } from './BaseButton';
+import { ButtonWithIcon } from './ButtonWithIcon';
 interface Props {
   open: boolean;
   setOpen: (open: boolean) => void;
-  date: string;
-  setDate: (date: string) => void;
+  date: Date | null;
+  setDate: (date: Date) => void;
   title: string;
   placeholder: string;
 }
 
 export function DatePickerModal({ open, setOpen, date, setDate, title, placeholder }: Props) {
+  const [datePicker, setDatePicker] = useState(false);
+
+  const handleDate = (date: Date) => {
+    setDate(date);
+    setDatePicker(false);
+  };
+
   return (
     <Modal visible={open} transparent animationType="fade">
-      <View className="absolute left-0 right-0 z-10 h-2/6 pb-2  my-14 mx-6 rounded-xl items-center  bg-primary_600 px-4 top-44">
+      <View className="absolute left-0 right-0 z-10  pb-8  my-14 mx-6 rounded-xl items-center  bg-primary_600 px-4 top-44">
         <View className="flex-row items-center w-full">
           <BackButton onPress={() => setOpen(false)} />
           <Text className="text-zinc-100 self-center text-2xl font-bold">{title}</Text>
         </View>
-        <BaseInput
-          error={[]}
-          name="observation"
-          onChangeText={setDate}
-          value={date}
-          containerStyle="w-full"
-          placeholder={placeholder}
+        <ButtonWithIcon
+          className=" justify-between bg-primary_400"
+          titleClassName="text-xl mr-4"
+          orientation="right"
+          onPress={() => setDatePicker(true)}
+          title={date ? moment(date).format('DD/MM/YYYY') : placeholder}
+          icon={<Feather name="calendar" size={28} color="white" />}
         />
-        <Button.Root
-          onPress={() => setOpen(false)}
-          className="self-center mt-8"
-          variant="confirmation"
-        >
-          <Button.Title title="Atualizar" />
-        </Button.Root>
+        <DateTimePickerModal
+          isVisible={datePicker}
+          mode="date"
+          isDarkModeEnabled={true}
+          onConfirm={(item: Date) => handleDate(item)}
+          onCancel={() => setDatePicker(false)}
+        />
+        <BaseButton variant="confirmation" title="Confirmar" onPress={() => setOpen(false)} />
       </View>
       <View className="flex 1 bg-zinc-900 h-full opacity-90" />
     </Modal>
