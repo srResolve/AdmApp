d35d9c7580bf88cc96db9c1b88c6dcf80b7b2aa4
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-04 09:16:25 -0400

    add option to customize title style

--- src/components/global/ButtonWithIcon.tsx
+++ src/components/global/ButtonWithIcon.tsx
@@ -6,16 +6,24 @@ interface Props extends TouchableOpacityProps {
   title: string;
   icon: ReactNode;
   orientation?: 'left' | 'right';
+  titleClassName?: string;
 }
 
-export function ButtonWithIcon({ title, icon, className, orientation = 'left', ...rest }: Props) {
+export function ButtonWithIcon({
+  title,
+  icon,
+  className,
+  titleClassName,
+  orientation = 'left',
+  ...rest
+}: Props) {
   return (
     <TouchableOpacity
       className={`p-2 bg-green-700 rounded-lg mt-2 ${orientation === 'left' ? 'flex-row' : 'flex-row-reverse'} items-center ${className}`}
       {...rest}
     >
       {icon}
-      <Text className="mx-1  text-zinc-100 font-bold">{title}</Text>
+      <Text className={`mx-1 text-zinc-100 ${titleClassName} font-bold`}>{title}</Text>
     </TouchableOpacity>
   );
 }
