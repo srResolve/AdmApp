5b784f64d9370eaf3ffebb36a3f5e82f83071510
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-06 17:21:22 -0400

    Create card to schedule table

---
+++ src/components/app/schedule/ScheduleTableCard.tsx
@@ -0,0 +1,37 @@
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import moment from 'moment';
+import { Text, View } from 'react-native';
+import { StatusCard } from '../../global/StatusCard';
+
+export function ScheduleTableCard({ item }: any) {
+  return (
+    <View className="flex-row justify-between p-1 px-2 items-center bg-primary_400 mt-2 rounded-lg border border-zinc-100">
+      <View className=" w-4/6">
+        <View className="flex-row items-center">
+          <AntDesign name="user" size={16} color="white" />
+          <Text className="ml-1 text-sm text-zinc-700">Cliente:</Text>
+          <Text className="ml-1 text-lg font-semibold text-zinc-100">{item.client.name}</Text>
+        </View>
+        <View className="flex-row items-center w-full">
+          <MaterialCommunityIcons name="account-cog-outline" size={16} color="white" />
+          <Text className="ml-1 text-sm text-zinc-700">Colaborador:</Text>
+          <Text
+            numberOfLines={1}
+            className=" ml-1 font-semibold text-lg text-zinc-100 flex-1 truncate"
+          >
+            {(item.user && item.user.name) || '-'}
+          </Text>
+        </View>
+        <View className="flex-row items-center">
+          <AntDesign name="clockcircleo" size={16} color="white" />
+          <Text className="ml-1 text-sm font-semibold text-zinc-100">
+            {moment(item.date).format('DD/MM/YYYY')} - {item.time}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <StatusCard status={item.status} />
+      </View>
+    </View>
+  );
+}
